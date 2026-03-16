1179acc30b6d6e8e69ddd4f1e4eb0717bcf428a0
forma1100 <forma1100@GOS-VDI1100>
2026-03-16 10:38:57 +0100

    Démo-Tableaux-Intro

---
+++ Demo-Tableau-01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo_Tableau_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "essential")
+            {
+                Console.WriteLine("Coucou les Essentials4IT!");
+            }
+            //int[] ints = { 1, 2, 3, 4 };
+            //int[] ints = [ 1, 2, 3, 4 ];
+            int[] ints = null;
+            ints = new int[4];
+            ints[0] = 1;
+            ints[1] = 2;
+            ints[2] = 3;
+            ints[3] = 4;
+            Console.WriteLine("Fin du programme");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo-Tableau-02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo_Tableau_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            bool[] tab = new bool[10];
+            tab[0] = true;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (tab[i] == true)
+                {
+                    Console.Write("P");
+                }
+                else
+                {
+                    Console.Write("-");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Que voulez-vous faire?\n(G)auche-(D)roite-(Q)uitter");
+            string choix = Console.ReadLine()!;
+        }
+    }
+}
